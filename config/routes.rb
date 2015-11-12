ba818d4b8ba9e297ec83e1b54598b5257638781a
Evan Berg <evan.m.berg@gmail.com>
2015-11-12 12:28:32 -0500

    this is a mess, lets restart shall we?

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :comments
+
   root "users#index"
   get "/users" => "users#index"
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141117163921) do
+ActiveRecord::Schema.define(version: 20151112172459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: true do |t|
+    t.text     "content"
+    t.string   "author"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "schema_info", id: false, force: true do |t|
     t.integer "version", default: 0, null: false
